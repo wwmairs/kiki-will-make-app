2bad2146cc60b9fae83ba25d800343968f5c4930
wwmairs <wwmairs@gmail.com>
2017-10-09 16:03:04 -0400

    trying to make more screens

--- TestProject/App.js
+++ TestProject/App.js
@@ -1,11 +1,22 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Hello World!</Text>
+      </View>
+    );
+  }
+}
+
+export default class Background extends Component {
+  render () {
+    return (
+      <View style={{flex:1}}>
+        <View style={{flex}}: 4, backgroundColor: 'powederblue'}}/>
+        <View style={{flex}}: 1, backgroundColor: 'grey'}}/>
       </View>
     );
   }
