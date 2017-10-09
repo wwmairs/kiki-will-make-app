0bd6f2e54e364131a7fe4912bd17da056cc61618
wwmairs <wwmairs@gmail.com>
2017-10-09 16:49:32 -0400

    a strait up react-native init project, but it actually works and is called the right thing

--- Walker/App.js
+++ Walker/App.js
@@ -1,37 +1,57 @@
-import React from 'react';
-import { StyleSheet, Text, TextInput, View } from 'react-native';
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
-// export default class App extends React.Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text>Hello World!</Text>
-//       </View>
-//     );
-//   }
-// }
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
 
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' +
+    'Cmd+D or shake for dev menu',
+  android: 'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
 
-export default class Background extends React.Component {
-  render () {
+export default class App extends Component<{}> {
+  render() {
     return (
-      <View style={{flex:1}}>
-        <View style={{flex: 4, backgroundColor: '#b0e0e6'}}/>
-        <View style={{flex: 1, backgroundColor: '#808080'}}/>
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to React Native!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit App.js
+        </Text>
+        <Text style={styles.instructions}>
+          {instructions}
+        </Text>
       </View>
     );
   }
 }
 
-export
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
   },
 });
-
-// AppRegistry.registerComponent('TestProject', () => FlexDimensionsBasics);
