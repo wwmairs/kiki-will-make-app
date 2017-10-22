db6c74a72ca572876a41e3ae43ad1cbb1bacf3b0
wwmairs <wwmairs@gmail.com>
2017-10-22 17:35:59 -0400

    linked react-native-contacts

--- Walker/android/app/src/main/java/com/walker/MainApplication.java
+++ Walker/android/app/src/main/java/com/walker/MainApplication.java
@@ -3,6 +3,7 @@ package com.walker;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rt2zz.reactnativecontacts.ReactNativeContacts;
 import com.airbnb.android.react.maps.MapsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +25,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new ReactNativeContacts(),
             new MapsPackage()
       );
     }
