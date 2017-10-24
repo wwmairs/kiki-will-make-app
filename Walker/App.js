38b8128f5f0c10c9bc9ec952b8e5b535de1b637c
Ki Ki Chan <hiu.chan@tufts.edu>
2017-10-24 14:53:18 -0400

    authenticate logged in user

--- Walker/App.js
+++ Walker/App.js
@@ -46,6 +46,7 @@ export class LoginScreen extends React.Component {
   };
   constructor(props) {
     super(props);
+//    this.unsubscribe = null;
     this.state = {email: '',
                   password: '',
                   error:'',
@@ -57,17 +58,18 @@ export class LoginScreen extends React.Component {
   };
 
    onLoginPress() {
+        const {navigate} = this.props.navigation;
         this.setState({ error: '', loading: true });
 
         const { email, password } = this.state;
         firebase.auth().signInWithEmailAndPassword(email, password)
-            .then(() => { this.setState({ error: '', loading: false }); })
+            .then(() => { this.setState({ error: '', loading: false });
+                          console.log("bitch i'm authenticated");
+              })
             .catch(() => {
                 //Login was not successful, let's create a new account
                 firebase.auth().createUserWithEmailAndPassword(email, password)
                     .then(() => { this.setState({ error: '', loading: false });
-                                console.log('creating new account la');
-                          
 
                      })
                     .catch(() => {
@@ -75,6 +77,17 @@ export class LoginScreen extends React.Component {
                     });
             });
     }
+
+  componentDidMount() {
+  const {navigate} = this.props.navigation;
+    this.unsubscribe = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        console.log("HI IM HERE TOOO BITCH IN COMPONENTDIDMOUNT")
+        console.log("what the fuck is user", user)
+        navigate('Directions')
+      }
+    })
+  }
 
   render () {
     const {navigate} = this.props.navigation;
@@ -156,9 +169,7 @@ export class SignupScreen extends React.Component {
                     .catch(() => {
                         this.setState({ error: 'Authentication failed.', loading: false });
                     });
-           
     }
-
 
   render () {
     const {navigate} = this.props.navigation;
@@ -194,9 +205,8 @@ export class SignupScreen extends React.Component {
                 secureTextEntry
                 onChangeText={(password) => this.setState({password})}
                 value = {this.state.password}
-
               />
-
+              
               <TouchableOpacity style={styles.buttonContainer}>
                   <Button
                   // onPress={() => navigate('Directions', {name: this.state.username})}
